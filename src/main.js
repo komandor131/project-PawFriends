133e0556024612dcb97038165bdff82d828ff35d
komandor <koletttut@gmail.com>
2026-03-03 15:16:31 +0200

    fix structure 2

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import 'izitoast';
+import 'axios';
+import 'swiper';
+import 'accordion-js';
+import 'star-rating.js';
+import 'raty-js';
+import 'sweetalert2';
+import 'basiclightbox';
+
+import.meta.glob('./js/*.js', { eager: true });
